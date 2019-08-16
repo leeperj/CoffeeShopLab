9b27eadda5f27a5d46d04ede230cc7672b6b8a14
unknown <josephleeper@gmail.com>
2019-08-16 15:09:20 -0400

    add coffeeshoplab

---
+++ CoffeeShopLab/Controllers/CoffeeShopController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShopLab.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopLab.Controllers
+{
+    public class CoffeeShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult UserForm()
+        {
+            return View(new User());
+        }
+
+
+        [HttpPost]
+        public IActionResult UserForm(User newUser)
+        {
+            return RedirectToAction("AddUser", newUser);
+        }
+
+        public IActionResult AddUser(User user)
+        {
+            return View(user);
+        }
+
+    }
+}
